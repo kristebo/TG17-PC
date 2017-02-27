0e419bb2fd3d0d1e4e97d7a9a2d17c5576025981
Adrian Helle <adriah@ifi.uio.no>
2017-02-27 11:10:35 +0100

    fixed run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --name="flaskapi" --network="host" -d -p 5000:5000 -e "FLASK_APP=/usr/src/app/flaskapi.py" -i tgpc/flaskapi flask run
+docker run --name="flaskapi" -d -p 5000:5000 -i tgpc/flaskapi gunicorn --bind 0.0.0.0:5000 --pythonpath /usr/src/ wsgi:app

e2dca44f66991b6d2d3611779a69da66ab7f356b
Adrian Helle <adriah@ifi.uio.no>
2017-02-28 16:08:01 +0100

    fixed the run.sh script

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+
+running=$(docker inspect --format="{{ .State.Running }}" flaskapi 2>/dev/null || true)
+if [ "$running" != "" ]; then
+    if [ "$running" == "true" ]; then
+        docker stop flaskapi
+    fi
+    docker rm flaskapi
+fi
+
 docker run --name="flaskapi" -d -p 5000:5000 -i tgpc/flaskapi gunicorn --bind 0.0.0.0:5000 --pythonpath /usr/src/ wsgi:app

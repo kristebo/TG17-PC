9496cd120be589c9dd9332a4cf6064b5f2a150cd
kristebo <krister.borge@ifi.uio.no>
2017-03-23 10:37:33 +0100

    reset tgpcdeliver

--- src/tgpcinn/tgpcDeliver.py
+++ src/tgpcinn/tgpcDeliver.py
@@ -1,14 +1,32 @@
 from flask import Flask, json, jsonify, Response, request, abort, os
 from functools import wraps
-from
-
 import csv, codecs
-app = Flask(__name__)
-key = '42'
+
+## http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
 
 #uploads
-UPLOAD_FOLDER = 'tg/TG17-PC/src/tgpcinn/uploads'
+UPLOAD_FOLDER = '../upload'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'py', 'go', 'c', 'sh', 'cpp', 'make',])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+app = Flask(__name__)
+key = '42'
+# read the directories
+def make_tree(path):
+    tree = dict(name=os.path.basename(path), children=[])
+    try: lst = os.listdir(path)
+    except OSError:
+        pass #ignore errors
+    else:
+        for name in lst:
+            fn = os.path.join(path, name)
+            if os.path.isdir(fn):
+                tree['children'].append(make_tree(fn))
+            else:
+                tree['children'].append(dict(name=name))
+    return tree
+
+
 
 def require_appkey(view_function):
     @wraps(view_function)
@@ -21,30 +39,52 @@ def require_appkey(view_function):
     return decorated_function
 
 
-@require_appkey
-def newuser():
-    partid=request.args.get('id')
-    os.makedirs('id')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 
 
 @app.route('tgpcdeliver/api/delivery/', methods=['GET', 'POST'])
 @require_appkey
 def delivery():
-    id=request.args.get('id')
-    partid=request.args.get('pid')
-    os.makedirs(id/partid)
+    id=request.args.get('_id')
+
 
 @app.route('/tgpc/api/getpoints/')
 @require_appkey
 def getpoint():
-    id=request.args.get('id')
-    partid=request.args.get('pid')
-        try: lst=os.listdirs()
-
+    id=request.args.get('_id')
 
 
 
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0',debug=True)
+    app.run()

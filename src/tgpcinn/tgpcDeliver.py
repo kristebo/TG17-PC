b1ee6a2a60551495264ecfdb5fb5fb02f71b9b34
kristebo <krister.borge@ifi.uio.no>
2017-03-27 12:37:58 +0200

    fix the tgpcdeliver

--- src/tgpcinn/tgpcDeliver.py
+++ src/tgpcinn/tgpcDeliver.py
@@ -1,95 +1,89 @@
-#!python2
-from flask import Flask, json, jsonify, Response, request, abort, redirect
-from functools import wraps
-import csv, codecs, os
-from werkzeug.utils import secure_filename
+import os
+# We'll render HTML templates and access data sent by POST
+# using the request object from flask. Redirect and url_for
+# will be used to redirect the user once the upload is done
+# and send_from_directory will help us to send/show on the
+# browser the file that the user just uploaded
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug import secure_filename
 
-## http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
-
-#uploads
-UPLOAD_FOLDER = '../upload'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'py', 'go', 'c', 'sh', 'cpp', 'make',])
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+# Initialize the Flask application
 app = Flask(__name__)
-key = '42'
-# read the directories
 
-def make_tree(path):
-    tree = dict(name=os.path.basename(path), children=[])
-    try: lst = os.listdir(path)
-    except OSError:
-        pass #ignore errors
-    else:
-        for name in lst:
-            fn = os.path.join(path, name)
-            if os.path.isdir(fn):
-                tree['children'].append(make_tree(fn))
-            else:
-                tree['children'].append(dict(name=name))
-    return tree
+# This is the path to the upload directory
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+# These are the extension that we are accepting to be uploaded
+app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
-
-
-def require_appkey(view_function):
-    @wraps(view_function)
-    # the new, post-decoration function. Note *args and **kwargs here.
-    def decorated_function(*args, **kwargs):
-        if request.args.get('key') and request.args.get('key') == key:
-            return view_function(*args, **kwargs)
-        else:
-            abort(401)
-    return decorated_function
-
-
+# For a given file, return whether it's an allowed type or not
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
+# This route will show a form to perform an AJAX request
+# jQuery is loaded to execute the request and update the
+# value of the operation
+@app.route('/')
+def index():
+    return """<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"
+              rel="stylesheet">
+      </head>
+      <body>
+        <div class="container">
+          <div class="header">
+            <h3 class="text-muted">How To Upload a File</h3>
+          </div>
+          <hr/>
+          <div>
 
-@app.route('/tgpcdeliver', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            print filename
-            file.save(os.path.join(UPLOAD_FOLDER, filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
+          <form action="upload" method="post" enctype="multipart/form-data">
+            <input type="file" name="file"><br /><br />
+            <input type="submit" value="Upload">
+          </form>
+          </div>
+        </div>
+      </body>
+    </html>"""
+def success():
+    return """<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"
+              rel="stylesheet">
+      </head>
+      <body>
+        <div class="container">
+          <div class="header">
+            <h3 class="text-muted">great success</h3>
+          </div>
+        </div>
+      </body>
+    </html>"""
 
+# Route that will process the file upload
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the name of the uploaded file
+    file = request.files['file']
+    # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+        filename = secure_filename(file.filename)
+        # Move the file form the temporal folder to
+        # the upload folder we setup
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Redirect the user to the uploaded_file route, which
+        # will basicaly show on the browser the uploaded file
+        return redirect(success())
 
-
-@app.route('/tgpcdeliver/api/delivery/', methods=['GET', 'POST'])
-@require_appkey
-def delivery():
-    id=request.args.get('_id')
-
-
-@app.route('/tgpcdeliver/api/getpoints/')
-@require_appkey
-def getpoint():
-    id=request.args.get('_id')
-
-
-
-if __name__ == '__main__':
-    app.run()
+# This route is expecting a parameter containing the name
+# of a file. Then it will locate that file on the upload
+# directory and show it on the browser, so if the user uploads
+# an image, that image is going to be show after the upload
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)

6e8ace8077ea45ae40cd3c0ebae3f63e85608f27
kristebo <krister.borge@ifi.uio.no>
2017-03-29 21:02:18 +0200

    remove som shit

--- src/tgpcinn/tgpcDeliver.py
+++ src/tgpcinn/tgpcDeliver.py
@@ -21,23 +21,10 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
-# This route will show a form to perform an AJAX request
-# jQuery is loaded to execute the request and update the
-# value of the operation
-# summary = request.args.get('summary', None) # use default value repalce 'None'
-# "localhost:5000/tgpc/api/?taskid=id&partid=id
-
-@app.route('/tgpc/api')
-def upload():
-    self.taskid=request.args.get('taskid', str)
-    self.partid=request.args.get('partid', str)
-    return render_template('upload.html')
-
-
 
 
 # Route that will process the file upload
-@app.route('/upload', methods=['POST', 'GET'])
+@app.route('/tgpc/upload', methods=['POST', 'GET'])
 def upload_file():
     # Get the name of the uploaded file
     file = request.files['file']
@@ -50,13 +37,7 @@ def upload_file():
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         # Redirect the user to the uploaded_file route, which
         # will basicaly show on the browser the uploaded file
-        return redirect(render_template('success.html'))
-
-# This route is expecting a parameter containing the name
-# of a file. Then it will locate that file on the upload
-# directory and show it on the browser, so if the user uploads
-# an image, that image is going to be show after the upload
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+        # return redirect(render_template('success.html'))
+    self.taskid=request.args.get('taskid', str)
+    self.partid=request.args.get('partid', str)
+    return render_template('upload.html')

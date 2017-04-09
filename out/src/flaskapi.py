8e6a8329bf5a039f97c2d3bfb583a8125abb3280
Adrian Helle <adriah@ifi.uio.no>
2017-04-09 21:18:04 +0200

    Remember to close files @kristebo

--- out/src/flaskapi.py
+++ out/src/flaskapi.py
@@ -81,6 +81,7 @@ def get_taskstate(partid):
     f = open("uploads/"+str(partid)+"/deliverd.txt", 'r')
     fields = ("task", "status")
     status_dict = csv.DictReader(f, fields)
+    f.close()
     for row in status_dict:
         statuslist.append(row)
     return jsonify({'taskstate':statuslist})
@@ -142,6 +143,7 @@ def getdeliveries(partid):
     fieldnames=['taskid', 'state']
     delifile= open("uploads/"+str(partid)+"/deliverd.txt", 'r')
     tasks=csv.DictReader(delifile, fieldnames)
+    delifile.close()
     taskspart=[]
     for row in tasks:
         taskspart.append(row)
